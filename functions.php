58ca303ed127448efbf258040403ec35b7055c12
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-09 12:32:33 +0100

    integration js

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
  function univerity_files(){
+    wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/build/index.js' ), array('jquery'), '1.0', true );
     wp_enqueue_style('custum-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri( '/build/style-index.css' ));
