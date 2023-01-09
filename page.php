d594a350cd0b47bf8e6ea64695389a685c7a891a
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-09 22:05:54 +0100

    Parent & Children pages

--- page.php
+++ page.php
@@ -13,11 +13,18 @@ while(have_posts()){
     </div>
 
     <div class="container container--narrow page-section">
+        <?php 
+        $parent = wp_get_post_parent_id( get_the_ID() );
+        if($parent){
+        ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent)?></a> <span class="metabox__main"><?php the_title()?></span>
         </p>
       </div>
+        <?php 
+        }
+        ?>
      <!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
