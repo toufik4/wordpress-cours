a6941ed85bd2daa26e65d503317c2b5a0be2ab76
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-11 09:57:52 +0100

    Amélioration metabox dans single.php

--- single.php
+++ single.php
@@ -14,7 +14,7 @@ while(have_posts()){
 <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog')?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main"><?php the_title()?></span>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog')?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('d/m/Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
         </p>
   </div>
 
