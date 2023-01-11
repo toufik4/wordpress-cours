5fa577e5a6d6255d040e5f6e1ffc9c7a377f73b8
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-11 11:53:50 +0100

    Custum query + 2 most recent posts

--- front-page.php
+++ front-page.php
@@ -46,10 +46,10 @@
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-              <p><?php the_excerpt();?> <a href="#" class="nu gray">Read more</a></p>
+              <p><?php echo wp_trim_words(get_the_content(),18); ?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
             </div>
           </div>
-          <?php }?>
+          <?php } ?>
 
           <p class="t-center no-margin"><a href="<?php echo site_url('/blog');?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
