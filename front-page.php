ef6f2b0ab764fa5ef1178c3e0c8f94c08743817d
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-11 11:54:13 +0100

    Custum query + wp_reset_postdata

--- front-page.php
+++ front-page.php
@@ -49,7 +49,7 @@
               <p><?php echo wp_trim_words(get_the_content(),18); ?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
             </div>
           </div>
-          <?php } ?>
+          <?php } wp_reset_postdata(); ?>
 
           <p class="t-center no-margin"><a href="<?php echo site_url('/blog');?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
