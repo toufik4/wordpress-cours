df5e842fb2e12267a25e6c7664764cad79b547e7
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2023-01-11 13:10:57 +0100

    effacer la creation post type event de functions.php

--- functions.php
+++ functions.php
@@ -13,14 +13,5 @@
  }
  add_action('after_setup_theme', 'univerity_features');
 
- function univerity_post_types(){
-   register_post_type( 'event', array(
-      'public' => true,
-      'menu_icon' => 'dashicons-calendar',
-      'labels' => array(
-         'name' => 'Events'
-      )
-   ) );
-}
-add_action('init', 'univerity_post_types');
+ 
 ?>
